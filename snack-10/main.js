698c31511e1aa05a11ffa093329672e5b15c291b
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-05-17 23:35:04 +0200

    snack 10

--- snack-10/main.js
+++ snack-10/main.js
@@ -1,7 +1,7 @@
 // Scrivere un programma a cui venga chiesto all'utente di inserire numeri uno dopo l'altro.
 // Quando l'utente scriverà 2 volte consecutive lo stesso numero allora il programma dovrà terminare.
 
-let num = '' //creo una variante con una stringa vuota
+let num = '' //creo una variabile con una stringa vuota
 
 //apro un ciclo for infinito che continua a chiedere all'utente numeri con prompt 'input'
 for (i=1; i>0;){
@@ -10,8 +10,11 @@ for (i=1; i>0;){
     //verifico che input sia un numero, altrimenti alert e fermo l'esecuzione
     if (isNaN(input)){ 
         alert('è necessario inserire un numero per giocare!');
-
-    } else if (input===num){     //se input è un numero, verifico che sia uguale alla variabile num (stringa vuota alla prima iterazione). se uguale, alert e fermo l'esecuzione
+        break
+    }
+    
+    //se input è un numero, verifico che sia uguale alla variabile num (stringa vuota alla prima iterazione). se uguale, alert e fermo l'esecuzione
+    if (input===num){      
         alert('hai inserito due numeri uguali');
         break
     }
