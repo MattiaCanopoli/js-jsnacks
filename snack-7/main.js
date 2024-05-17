9c86f5287c0cd8fbbe6879ad1f9bdaa8b0716627
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-05-17 22:45:04 +0200

    snack 7.1

---
+++ snack-7/main.js
@@ -0,0 +1,6 @@
+// Scrivere un programma che stampi la tabellina del 2, fino al numero 1000.
+// Modificate poi il programma in modo che venga chiesto all'utente il numero massimo consentito, e stampare tutta la tabellina del 2 fino al numero inserito.
+
+for (let i = 1; i<=1000; i++) {
+    console.log(i*2)
+}
